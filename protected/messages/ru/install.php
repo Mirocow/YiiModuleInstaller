11ca9c74e6db3054d3668a9f64f30bd081225ec2
horechek <horechek@gmail.com>
2012-06-13 18:36:25 +0300

    новая плюшка: в папку install можно положить файлы install.sql и uninstall.sql которые выполнятся в нужный момент. теперь модули можно удалять совсем. включена мультиязычность. включенны flash messages

---
+++ protected/messages/ru/install.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	'Install' => 'Инсталировать',
+	'Uninstall' => 'Деинсталировать',
+	'Not removed' => 'Не получилось удалить',
+	'Modue removed' => 'Модуль удален',
+	'Modue installed' => 'Модуль успешно установлен',
+	'Modue builded' => 'Модуль упакован'
+);
---
+++ uploads/raiting/manifest.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+	'title' => 'Rating Module',
+	'description' => 'Module for manage users in application.',
+	'version' => '0.1.0'
+);
